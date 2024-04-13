8905fe32faa6ea18d41471f0d1fb2c0fe4f7eb18
newwares <45801973+waresnew@users.noreply.github.com>
2024-04-13 19:53:02 -0400

    change header

--- HIDEvent.h
+++ HIDEvent.h
@@ -1,6 +1,7 @@
-@class NSArray;
-
+#import <Foundation/Foundation.h>
 @interface HIDEvent : NSObject
+@end
+@interface HIDEvent (HIDEventDesc) <NSCopying>
 + (id)ambientLightSensorEvent:(unsigned long long)arg1 level:(unsigned int)arg2 options:(unsigned int)arg3;
 - (void)setAmbientLightSensorIlluminance:(double)arg1;
 @end
